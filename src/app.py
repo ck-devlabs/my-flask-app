ca996b5380906298e83b8b3f597272031eb1bda2
ck-devlabs <chandrakant.devlabs@gmail.com>
2024-01-18 16:26:10 -0500

    first commit

--- src/app.py
+++ src/app.py
@@ -18,4 +18,4 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "Hello, " + input_text + "! Hoe you are doing great!</br> Thanks for reviewing my assignment, Please rate it well! :)"
+    return "Hello, " + input_text + "! Hope you are doing great!</br> Thanks for reviewing my assignment, Please rate it well! :)"
