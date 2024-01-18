9a2c64ab04e9dd840c4f65ba3092af782c0cc1cc
ck-devlabs <chandrakant.devlabs@gmail.com>
2024-01-18 16:15:59 -0500

    first commit

--- untitled folder/src/app.py
+++ untitled folder/src/app.py
@@ -9,11 +9,12 @@ def main():
     return '''
     Enter your name to get a greeting:
      <form action="/echo_user_input" method="POST">
-         <input name="user_input" placeholder="Please enter name.">
+         <input name="user_input" placeholder="Jack">
          <input type="submit" value="Submit!">
      </form>
      '''
+
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "Hello, " + input_text + "! Hoe you are doing great!</br> Thanks for reviewing my assignment, Please rate it well! :)"
+    return "Greetings, " + input_text + "! How are you?<br/>Please rate my assignment well!<br/>Thanks"
